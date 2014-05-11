aacd77e5d4dc7a2cc3f9809ef35a5867df4594d6
Donatus Kamau <donatus.kamau@gmail.com>
2014-05-12 01:34:25 +0300

    Added and styled header and footer section

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HTML Sample work</title>
 
-    <link href="_/css/bootstrap.min.css" rel="stylesheet">
-    <link href="_/css/my_styles.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/my_styles.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,9 +17,33 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
-    <script src="_/js/bootstrap.js"></script>
-    <script src="_/js/myscripts.js"></script>
+    <header>
+        <div class="content-wrapper">
+            <div class="ribbon">
+                Tee ise
+            </div>
+            <div class="logo">
+                <div id="tag">Imeline</div>Ajalugu
+            </div>
+            <div class="content">
+                Vali nende põnevate lugude seast välja enda lemmikud. <br>
+                Sinu ja teiste lugejate soovide põhjal paneme kokku ajakirja novembri numbri. 
+                Nii sünnib Eesti esimene lugejate poolt kokku pandud ajakiri, mille Sina võid saada endale tasuta!
+                <div class="cta">
+                    Vali oma lemmikartiklid
+                </div>
+            </div>
+        </div>
+    </header>
+    <footer>
+        <div class="content-wrapper">
+            <div class="footer-text">
+                <p><a href="#">Tutvu kampaania tingimustega</a></p>
+                <p>Klienditeeninduse e-post: <a href="mailto:register@imelineajalugu.ee">register@imelineajalugu.ee</a>, tel: 667 0099 (tööpäeviti 9–17)  </p>
+            </div>
+        </div>
+    </footer>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/myscripts.js"></script>
   </body>
 </html>
