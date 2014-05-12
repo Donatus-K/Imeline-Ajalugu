42b3b8fba6e6d51b92dfff35dfaf7e3d2b70f653
Donatus Kamau <donatus.kamau@gmail.com>
2014-05-12 17:47:40 +0300

    Added accordion on articles, Added lower content

--- index.php
+++ index.php
@@ -35,6 +35,74 @@
             </div>
         </div>
     </header>
+    <section class="intro">
+        <div class="content-wrapper">
+            <h3>Pane kokku omaenda Imeline Ajalugu</h3>
+            <p>
+                Vali igast teemast kuni 3 artiklit Imelise Ajaloo novembri numbrisse. Tänutäheks saadame selle Sulle tasuta.<br>
+                Kui Sul endal on ajakiri juba tellitud, telli tasuta number heale sõbrale, kolleegile või sugulasele.
+            </p>
+        </div>
+    </section>
+    <div class="content-wrapper">
+        <div id="articles">
+            <div class="panel clearfix">
+                <div class="clearfix" data-toggle="collapse" data-target="#article-1" data-parent="#articles">
+                    <span class="heading"><span class="glyphicon glyphicon-chevron-down"></span>Pingelised sündmused</span>
+                    <span class="position">2/3</span>
+                </div>
+
+                <div id="article-1" class="collapse in">
+                    Vali igast teemast kuni 3 artiklit Imelise Ajaloo novembri numbrisse. Tänutäheks saadame selle Sulle tasuta.<br>
+                        Kui Sul endal on ajakiri juba tellitud,
+                </div>
+            </div>
+            <div class="panel clearfix">
+                <div class="clearfix" data-toggle="collapse" data-target="#article-2" data-parent="#articles">
+                    <span class="heading"><span class="glyphicon glyphicon-chevron-down"></span>Kuulsad teod</span>
+                    <span class="position">0/3</span>
+                </div>
+
+                <div id="article-2" class="collapse">
+                    Vali igast teemast kuni 3 artiklit Imelise Ajaloo novembri numbrisse. Tänutäheks saadame selle Sulle tasuta.<br>
+                        Kui Sul endal on ajakiri juba tellitud,
+                </div>
+            </div>
+            <div class="panel clearfix">
+                <div class="clearfix"  data-toggle="collapse" data-target="#article-3" data-parent="#articles">
+                    <span class="heading"><span class="glyphicon glyphicon-chevron-down"></span>Igapäevane elu</span>
+                    <span class="position">0/3</span>
+                </div>
+
+                <div id="article-3" class="collapse">
+                    Vali igast teemast kuni 3 artiklit Imelise Ajaloo novembri numbrisse. Tänutäheks saadame selle Sulle tasuta.<br>
+                        Kui Sul endal on ajakiri juba tellitud,
+                </div>
+            </div>
+            <div class="panel clearfix">
+                <div class="clearfix"  data-toggle="collapse" data-target="#article-4" data-parent="#articles">
+                    <span class="heading"><span class="glyphicon glyphicon-chevron-down"></span>Suured konfliktid</span>
+                    <span class="position">0/3</span>
+                </div>
+
+                <div id="article-4" class="collapse">
+                    Vali igast teemast kuni 3 artiklit Imelise Ajaloo novembri numbrisse. Tänutäheks saadame selle Sulle tasuta.<br>
+                        Kui Sul endal on ajakiri juba tellitud,
+                </div>
+            </div>
+      </div>
+    </div>
+    <div class="content-wrapper">
+        <div class="done">Valmis!</div>
+        <div class="lower-content">
+            <em>Imeline Ajalugu</em> on põhjamaade populaarseim ajalooajakiri, millel on Eestis üle 50 000 lugeja. 
+            Ajakiri ilmub kord kuus, igas numbris on põnevad ja rikkalikult illustreeritud lood ligi 100 leheküljel. 
+            Imelist Ajalugu annab Eestis välja AS Äripäev alates 2011 aaasta sügisest.
+        </div>
+        <div class="image">
+            <img src="images/imeline-ajalugu.jpg" class="img-responsive" alt="Imeline Ajalugu">
+        </div>
+    </div>
     <footer>
         <div class="content-wrapper">
             <div class="footer-text">
